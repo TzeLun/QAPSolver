28ed45d2865bb3853602d32afa5fcbf8dbc7cea5
TzeLun <70504671+TzeLun@users.noreply.github.com>
2022-01-16 15:19:23 +0800

    Update comments

--- Demo.cpp
+++ Demo.cpp
@@ -10,10 +10,10 @@ void demo() {
 	std::string path = "had12.txt";
 
 	// Initialization of matrices, arrays and other variables
-	std::vector<std::vector<int>> F;											// flow matrix
-	std::vector<std::vector<int>> D;											// distance matrix
-	std::vector<int> initial;													// initial solution (permutation)
-	unsigned int size;															// = initial.size();
+	std::vector<std::vector<int>> F;					// flow matrix
+	std::vector<std::vector<int>> D;					// distance matrix
+	std::vector<int> initial;						// initial solution (permutation)
+	unsigned int size;							// = initial.size();
 
 	// Load the data and process it into F and D matrices
 	preprocessing(&path, &F, &D, &size);
@@ -22,8 +22,8 @@ void demo() {
 	QAPSolver model(&F, &D, &size);
 
 	// Generate initial solution and solve using the available algorithms
-	model.genInitSol("random", &initial, 100);									// generate initial solution with IRG, comment this out if using GA
-	model.TS(initial, 50, 7, 10);												// solve using algorithm, in this case Tabu Search
-	model.prtSolution();														// print the best permutation, solution and run time
+	model.genInitSol("random", &initial, 100);				// generate initial solution with IRG, comment this out if using GA
+	model.TS(initial, 50, 7, 10);						// solve using algorithm, in this case Tabu Search
+	model.prtSolution();							// print the best permutation, solution and run time
 
 }
